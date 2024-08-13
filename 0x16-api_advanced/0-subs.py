60346e9a8b12c875fa9ed5176a0e6df3dd47bce1
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-08-13 22:27:11 +0300

    Hot Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
-#!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-reddit-subscriber-checker/0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+if __name__ == '__main__':
+    subreddit = "programming"
+    print(f"{subreddit} has {number_of_subscribers(subreddit)} subscribers.")

aff113c8f8d2a774bd88d7755106e8e9e0ac5dc8
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-08-13 22:35:38 +0300

    Hot Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'my-reddit-subscriber-checker/0.1'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-if __name__ == '__main__':
-    subreddit = "programming"
-    print(f"{subreddit} has {number_of_subscribers(subreddit)} subscribers.")
+    results = response.json().get("data")
+    return results.get("subscribers")

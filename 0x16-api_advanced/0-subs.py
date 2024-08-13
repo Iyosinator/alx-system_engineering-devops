11a0d69449476202f25ac0a5dce99fde95f94048
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-08-13 22:21:36 +0300

    Hot Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,16 @@
 import requests
 
-def number_of_subscribers(subreddit):
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
 
-    return sub_info.json().get("data").get("subscribers")
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
+
+
 
 
 
